bab61083f4ae84902ea39ed5b5abbbf16e2bdb96
ddmoney420 <ddmoney420@users.noreply.github.com>
2026-01-25 21:41:28 -0700

    Sync Formula checksums

--- Formula/moji.rb
+++ Formula/moji.rb
@@ -7,20 +7,20 @@ class Moji < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ddmoney420/moji/releases/download/v#{version}/moji_darwin_arm64.tar.gz"
-      sha256 "9f77efcd3fa132ae59c9db43625c53e6da0fc280a3ad2591946bae1f859e7fb4"
+      sha256 "ab104fdd2d7fc7bfc33464c6d0651a297e5c61e69172697caeb283effa20d209"
     else
       url "https://github.com/ddmoney420/moji/releases/download/v#{version}/moji_darwin_amd64.tar.gz"
-      sha256 "f3e4428ed51c4c36b4da520219e5c87f26009cfc768d423da83fce4ec3f3bcc1"
+      sha256 "eb1f6cc94388879fb5942d4dbbd69f595f9ba208a324c501c1b700c8c026ce70"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/ddmoney420/moji/releases/download/v#{version}/moji_linux_arm64.tar.gz"
-      sha256 "a34c4c2c8bd6aba8e2245bbcbd37e4dd7eabae903bf14619b8ba8b6b1d276149"
+      sha256 "d2e6048fe3f28df9dfa31215da1d900498e1f25aac0521b9efc5c7f54f039c67"
     else
       url "https://github.com/ddmoney420/moji/releases/download/v#{version}/moji_linux_amd64.tar.gz"
-      sha256 "3931153c18786eac82fbb5cc03f73c90917e7570b406477f74e4d51d50d8148f"
+      sha256 "3d61635dd8a3b018b1c7d9a61a8a192d888975b4063ef0e348b73e72a537173e"
     end
   end
 
